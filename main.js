9e3ad03949d945eaed2fa8e6f720dc2c3656dd22
Maria Shorina <maryss11@yandex.ru>
2016-04-26 17:20:08 +0300

    second screen started

--- main.js
+++ main.js
@@ -1,9 +1,51 @@
 $(function(){
 
+    function checkScroll(){
+        var scrollTop = $('body').scrollTop();
+        console.log(scrollTop);
+
+
+        $('body')[scrollTop>=700 ? 'addClass' : 'removeClass']('no-transition');
+
+
+        $('body')[scrollTop>300 ? 'addClass' : 'removeClass']('scrolled');
+
+        $('#header-main')[scrollTop>=980 ? 'addClass' : 'removeClass']('scrolled');
+
+    };
+
+    checkScroll();
+
     $(window).scroll(function(){
-       var scrollTop = $('body').scrollTop();
-        console.log($('#content').scrollTop());
-       $('body')[scrollTop>0 ? 'addClass' : 'removeClass']('scrolled');
+       checkScroll();
+
     });
 
+
+
+
+
+    /*
+    var $window = $(window);		//Window object
+
+    var scrollTime = 1.2;			//Scroll time
+    var scrollDistance = 170;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
+
+    $window.on("mousewheel DOMMouseScroll", function(event){
+
+        event.preventDefault();
+
+        var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+        var scrollTop = $window.scrollTop();
+        var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+
+        TweenMax.to($window, scrollTime, {
+            scrollTo : { y: finalScroll, autoKill:true },
+            ease: Power1.easeOut,	//For more easing functions see http://api.greensock.com/js/com/greensock/easing/package-detail.html
+            autoKill: true,
+            overwrite: 5
+        });
+
+    });
+    */
 })
